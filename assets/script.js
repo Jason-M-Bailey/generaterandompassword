8e97ca9089ecfdce19ad171184f1f41491bb1599
Jason Bailey <jmbailey816@gmail.com>
2021-05-13 18:15:24 -0400

    Code format cleanup

--- assets/script.js
+++ assets/script.js
@@ -9,31 +9,34 @@ var special = "!@,#$%&*{}[]/\\+=";
 
 var chosenCharacters = ""; // this is a bucket where the other arrays are added, then we'll use a charAt to randomly pick characters from the string
 
-// write password function 
-function writePassword(){
+// write password function
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); // find the id#password in index.html
   passwordText.value = password; // paste the randomly generated password in the id#password field
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
 
 //generate password function gets called in writePassword function, and it should return the final password
-function generatePassword(){
-
+function generatePassword() {
   //do random generation here and return the final password in the end
   var result = "";
 
   //ask the user how many characters they want
-  var length = prompt("How many characters in your random password? (between 8 and 128)");
-  if(isNaN(length)){
-    alert("Come on! That's not even a number! Enter a number between 8 and 128, please.");
-    return generatePassword() // return to start if user enters a Not a Number
+  var length = prompt(
+    "How many characters in your random password? (between 8 and 128)"
+  );
+  if (isNaN(length)) {
+    alert(
+      "Come on! That's not even a number! Enter a number between 8 and 128, please."
+    );
+    return generatePassword(); // return to start if user enters a Not a Number
   }
-  if(length < 8 || length > 128){
+  if (length < 8 || length > 128) {
     alert("That wasn't between 8 and 128, try again!");
-    return generatePassword() // return to start if user enters less than 8 or greater than 128
+    return generatePassword(); // return to start if user enters less than 8 or greater than 128
   }
 
   //ask the user if they want to include upper, lower, num, special?
@@ -43,37 +46,38 @@ function generatePassword(){
   var includeSpecial = confirm("Include special characters?");
 
   // if the user clicks cancel on all character types!
-  if(!includeUpper && !includeLower && !includeNumbers && !includeSpecial){
-    alert("How are you going to create a password with no character types? Start over!");
-    return generatePassword() // is there a way to start this at -var includeUpper...?
+  if (!includeUpper && !includeLower && !includeNumbers && !includeSpecial) {
+    alert(
+      "How are you going to create a password with no character types? Start over!"
+    );
+    return generatePassword(); // is there a way to start this at -var includeUpper...?
   }
-  if(includeUpper){
-    chosenCharacters += upper
+  if (includeUpper) {
+    chosenCharacters += upper;
   }
-  if(includeLower){
-    chosenCharacters += lower
+  if (includeLower) {
+    chosenCharacters += lower;
   }
-  if(includeNumbers) {
-    chosenCharacters += numbers
+  if (includeNumbers) {
+    chosenCharacters += numbers;
   }
-  if(includeSpecial) {
-    chosenCharacters += special
+  if (includeSpecial) {
+    chosenCharacters += special;
   }
-for (var i = 0; i < length; i++) {
-  result += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
+  for (var i = 0; i < length; i++) {
+    result += chosenCharacters.charAt(
+      Math.floor(Math.random() * chosenCharacters.length)
+    );
+  }
+  return result;
 }
-return result;
-}
-
-
 
 // copy to clipboard
 var copyBtn = document.getElementById("copy"); //gets the id: #copy from index.html
 
-copyBtn.addEventListener("click", () => { //when the button is clicked do something 
-
-  password.select(); // this is defined earlier by id #password as the text box, so select the contents of this text box 
-
-  document.execCommand("copy"); // copies to clipboard 
+copyBtn.addEventListener("click", () => {
+  //when the button is clicked do something
+  password.select(); // this is defined earlier by id #password as the text box, so select the contents of this text box
+  document.execCommand("copy"); // copies to clipboard
   alert("Password Copied"); // creates popup at top of screen with message
 });
